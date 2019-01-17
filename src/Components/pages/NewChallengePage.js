f9dfe4b608d2dd273089f6b2efaede738a9a8e8c
James Phong Duong <james.duong93@gmail.com>
2019-01-18 10:30:02 +1100

    commit before merge

--- src/Components/pages/NewChallengePage.js
+++ src/Components/pages/NewChallengePage.js
@@ -1,35 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 
-const fs = require("fs");
-const youtube = require("./config/youtube")
+// const fs = require("fs");
+// const youtube = require("./../../config/youtube");
 
-const NewChallengePage = () => {
-    const onUploadSubmit = () => {
-        youtube.videos.insert({
-            snippet: {
-                title: "testing API upload",
-                description: "test desc"
-            },
-            media: {
-                body: fs.createReadStream("Untitled.mov"),
-            },
-            part: "snippet"
-            })
-            .then(response => console.log(response))
-            .catch(err => console.log(err));
-        }
-
+class NewChallengePage extends Component {
+    render() {
+    // const onUploadSubmit = () => {
+    // console.log(event.target.files[0]);
+        // youtube.videos.insert({
+        //     snippet: {
+        //         title: "testing API upload",
+        //         description: "test desc"
+        //     },
+        //     media: {
+        //         body: fs.createReadStream("Untitled.mov"),
+        //     },
+        //     part: "snippet"
+        //     })
+        //     .then(response => console.log(response))
+        //     .catch(err => console.log(err));
     return (
         <div>
             <h1> Create New Challenge </h1>
-            <form>
-                <input type="file" />
+            <form onSubmit= {(event) => {
+                event.preventDefault();
+                 const input = document.querySelector("input[type=file]");
+                 console.log(input);
+                    }   
+                        }>
                 <div>
-                    <input type="submit" value="upload" accept="video/*" onSubmit = {onUploadSubmit}/>
+                    <input type="text" placeholder="Title of the Challenge"/>
+                </div>
+                <div>
+                    <input type="text" placeholder="How the challenge should be completed"/>
+                </div>
+                <div>
+                    <input type="file" />
+                </div>
+                <div>
+                    <input type="submit" value="upload" accept="video/*" id="video" />
                 </div>
             </form>
         </div>
-    );
+        );
+    }
 }
 
 export default NewChallengePage;
