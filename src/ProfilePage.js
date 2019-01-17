76c0574eaf5f9104903d30b8040ed3ef51b5f305
Tyson Hofstetter <tysonh652@gmail.com>
2019-01-17 15:58:26 +1100

    done ProfileInfoCard component

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
+import ProfileInfoCard from './components/ProfileInfoCard';
+
 
 class Profile extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        <h1>Profile Page</h1>
+        <ProfileInfoCard />
       </div>
       
     );
