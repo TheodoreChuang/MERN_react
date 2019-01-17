ac7917fb09df731bf1f6b12649fb72ecd00c360b
Tyson Hofstetter <tysonh652@gmail.com>
2019-01-17 12:14:49 +1100

    done basic navbar and routes for feed and profile page

---
+++ src/FeedPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <h1>Feed Page</h1>
+      </div>
+     
+    );
+  }
+}
+
+export default Profile;
