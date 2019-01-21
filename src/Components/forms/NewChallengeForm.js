20a33d32b6694db7dbe19d56db0ffd532565c96b
James Phong Duong <james.duong93@gmail.com>
2019-01-21 22:10:40 +1100

    newchallengeform adjusted to redux form

--- src/Components/forms/NewChallengeForm.js
+++ src/Components/forms/NewChallengeForm.js
@@ -1,40 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
 import LocalApi from "../../apis/local";
+import { Field, reduxForm } from "redux-form";
+import Input from "./fields/Input";
 
-const NewChallengeForm = () => {
-    return (
-        <form onSubmit = {(event) => {
-            event.preventDefault();
-            const file = document.getElementById("uploadvideo").files[0]; 
-            const title = document.getElementById("title").value;
-            const desc = document.getElementById("desc").value;
+class NewChallengeForm extends Component {
+    onUploadSubmit = (formValues) => {
+        const { title, desc, video } = formValues;
+        this.uploadFile(video[0], title, desc);
+    }
 
-            uploadFile(file, title, desc);
-            }} encType="multipart/form-data">
-            <div>
-                <input type="text" placeholder="title" name="title" id="title" />
-            </div>
-            <div>
-                <input type="text" placeholder="description of challenge" name="desc" id="desc" />
-            </div>
-            <div>
-                <input type="file" id="uploadvideo" name="video" />
-            </div>
-            <div>
-                <input type="submit" />
-            </div>
-        </form>
-    );
+    uploadFile = (file, title, desc) => {
+        const fd = new FormData();
+        fd.append("video", file);
+        fd.append("title", title);
+        fd.append("desc", desc);
+    
+        try {
+            LocalApi.post("/challenges/upload", fd);
+            // if (error) throw error;
+    
+            alert("Succesfully submitted");
+    
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    render() {
+        const { handleSubmit } = this.props;
+
+        return (
+            <form onSubmit= {handleSubmit(this.onUploadSubmit)} encType="multipart/form-data">
+                <div>
+                    <Field
+                    name="title"
+                    component={Input}
+                    placeholder="title"
+                    type="text"
+                    />
+                </div>
+                <div>
+                    <Field
+                    name="desc"
+                    component={Input}
+                    placeholder="description of challenge"
+                    type="text"
+                    />
+                </div>
+                <div>
+                    <Field
+                    name="video"
+                    component={Input}
+                    type="file"
+                    />
+                </div>
+                <div>
+                    <Field
+                    type="submit"
+                    component={Input}
+                    value="submit"
+                    />
+                </div>
+            </form>
+        );
+    }
 }
 
-//upload file function
-const uploadFile = (file, title, desc) => {
-    const fd = new FormData();
-    fd.append("video", file);
-    fd.append("title", title);
-    fd.append("desc", desc);
+const WrappedNewChallengeForm = reduxForm({
+    form: "upload"
+})(NewChallengeForm);
 
-    LocalApi.post("/challenges/upload", fd);
-}
-
-export default NewChallengeForm;
+export default WrappedNewChallengeForm;
--- src/Components/forms/fields/Input.js
+++ src/Components/forms/fields/Input.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Input = ({meta, input, type, placeholder}) => {
+const Input = ({meta, input, type, placeholder, id, value, name}) => {
     return (
         <span>
-            <input {...input} autoComplete="false" type={type}  placeholder={placeholder}/>
+            <input {...input} autoComplete="false" type={type} id={id} placeholder={placeholder} value={value} name={name}/>
             <span> {meta.touched && meta.error} </span>
         </span>
     );
