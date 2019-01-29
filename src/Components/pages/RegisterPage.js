6893aa738f2376a995b9175b7433e58c55c5afea
Theodore Chuang <chuangtheodore@gmail.com>
2019-01-30 00:42:53 +1100

    Update proxy to point to Heroku Node server

--- src/Components/pages/RegisterPage.js
+++ src/Components/pages/RegisterPage.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import RegisterForm from "./../forms/RegisterForm";
-import { Link } from "react-router-dom";
 import "./RegisterPage.css";
-class RegisterPage extends Component  {
-    render() {
-        return (
-
-            <div className="container">
-                <h2> oneup </h2>
-                <RegisterForm history={this.props.history}/>
-            </div>
-        );
-    }
+class RegisterPage extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2> oneup </h2>
+        <RegisterForm history={this.props.history} />
+      </div>
+    );
+  }
 }
 export default RegisterPage;
