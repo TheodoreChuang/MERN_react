dd95dd7342ba6d98fdde042ea4c286503dba31aa
Theodore Chuang <chuangtheodore@gmail.com>
2019-01-29 16:59:15 +1100

    Initial Deployment: config API url

--- src/apis/local.js
+++ src/apis/local.js
@@ -3,18 +3,19 @@ import store from "./../store";
 
 //creates instance of axios
 const LocalApi = axios.create({
-    baseURL: "http://localhost:3000"
+  baseURL: "http://1up-app-dev.ap-southeast-2.elasticbeanstalk.com/"
+  // baseURL: "http://localhost:3000"
 });
 
 LocalApi.interceptors.request.use(function(config) {
-    const state = store.getState();
-    const token = state.auth.token;
+  const state = store.getState();
+  const token = state.auth.token;
 
-    if (token) {
-        config.headers['Authorization'] = `Bearer ${token}`
-    }
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
 
-    return config;
-})
+  return config;
+});
 
 export default LocalApi;
