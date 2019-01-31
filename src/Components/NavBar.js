aa9d3289df0095c93a26835f1f607f0100f11f89
James Phong Duong <james.duong93@gmail.com>
2019-02-01 09:52:18 +1100

    commit before overwriting origin james branch

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -36,40 +36,40 @@ const styles = theme => ({
   grow: {
     flexGrow: 1
   },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20
-  },
-  title: {
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block"
-    }
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
-    },
-    marginRight: theme.spacing.unit * 2,
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing.unit * 3,
-      width: "auto"
-    }
-  },
-  searchIcon: {
-    width: theme.spacing.unit * 9,
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  },
+  // menuButton: {
+  //   marginLeft: -12,
+  //   marginRight: 20
+  // },
+  // title: {
+  //   display: "none",
+  //   [theme.breakpoints.up("sm")]: {
+  //     display: "block"
+  //   }
+  // },
+  // search: {
+  //   position: "relative",
+  //   borderRadius: theme.shape.borderRadius,
+  //   backgroundColor: fade(theme.palette.common.white, 0.15),
+  //   "&:hover": {
+  //     backgroundColor: fade(theme.palette.common.white, 0.25)
+  //   },
+  //   marginRight: theme.spacing.unit * 2,
+  //   marginLeft: 0,
+  //   width: "100%",
+  //   [theme.breakpoints.up("sm")]: {
+  //     marginLeft: theme.spacing.unit * 3,
+  //     width: "auto"
+  //   }
+  // },
+  // searchIcon: {
+  //   width: theme.spacing.unit * 9,
+  //   height: "100%",
+  //   position: "absolute",
+  //   pointerEvents: "none",
+  //   display: "flex",
+  //   alignItems: "center",
+  //   justifyContent: "center"
+  // },
   inputRoot: {
     color: "inherit",
     width: "100%"
@@ -207,6 +207,7 @@ class PrimarySearchAppBar extends Component {
               noWrap
             >
               1UP
+
             </Typography>
             <div className={classes.grow} />
             <div className={classes.sectionDesktop}>
